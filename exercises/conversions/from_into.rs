59f736789527fd111e9e48462364254703007944
hiltay <904108079@qq.com>
2023-09-15 10:55:35 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,24 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0{
+            Person::default()
+        }else{
+            let words = s.split(',').collect::<Vec<&str>>();
+            if words.len() < 2 || words[0] == ""{
+                Person::default()
+            }else{
+                if let Ok(val) = words[1].parse::<usize>(){
+                    Person{name: words[0].to_owned(), age:val}
+                }else{
+                    Person::default()
+                }
+            }
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,25 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            Err(ParsePersonError::Empty)
+        } else {
+            let words = s.split(',').collect::<Vec<&str>>();
+            if words.len() != 2 {
+                Err(ParsePersonError::BadLen)
+            } else {
+                if words[0] == "" {
+                    return Err(ParsePersonError::NoName);
+                }
+                match words[1].parse::<usize>() {
+                    Ok(val) => Ok(Person {
+                        name: words[0].to_owned(),
+                        age: val,
+                    }),
+                    Err(err) => Err(ParsePersonError::ParseInt(err)),
+                }
+            }
+        }
     }
 }
 
